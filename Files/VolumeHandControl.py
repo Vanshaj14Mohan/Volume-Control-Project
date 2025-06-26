6086324c7d023e7a547ad04b8dcf547dd9e29315
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-06-26 19:55:02 +0530

    Files

--- Files/VolumeHandControl.py
+++ Files/VolumeHandControl.py
@@ -13,18 +13,22 @@ cap.set(3, weightCam)
 cap.set(4, heightCam)
 pTime = 0
 
-detector = htm.HandDetector()
+detector = htm.HandDetector(detectionCon=0.7)
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[2])
 
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-
     cv2.putText(img, f"FPS:{int(fps)}", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
+
+    
