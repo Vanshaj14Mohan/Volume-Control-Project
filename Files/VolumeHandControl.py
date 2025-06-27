efb4b4102da3c86aabb3fb7205469fd54e518337
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-06-27 14:33:12 +0530

    Files

--- Files/VolumeHandControl.py
+++ Files/VolumeHandControl.py
@@ -2,6 +2,10 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 
 ########## Camera Setting
@@ -15,22 +19,42 @@ pTime = 0
 
 detector = htm.HandDetector(detectionCon=0.7)
 
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+# volume = interface.QueryInterface(IAudioEndpointVolume)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volume.GetVolumeRange()
+print(volume.GetVolumeRange())
+volume.SetMasterVolumeLevel(0, None)
+
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        print(lmList[4], lmList[8]) #For thumb and index finger
+
+        #print(lmList[4], lmList[8]) #For thumb and index finger
 
         x1, y1 = lmList[4][1], lmList[4][2] #First element as x and second element as y
         x2, y2 = lmList[8][1], lmList[8][2]
         cx, cy = (x1+x2) // 2, (y1+y2) // 2
 
         #Draw circle on thumb and index point
-        cv2.circle(img, (x1,y1), 12, (255,0,255), cv2.FILLED)
-        cv2.circle(img, (x2,y2), 12, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (x1,y1), 10, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (x2,y2), 10, (255,0,255), cv2.FILLED)
         cv2.line(img, (x1,y1), (x2,y2), (255,0,255),3)
-        cv2.circle(img, (cx,cy), 12, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (cx,cy), 10, (255,0,255), cv2.FILLED) #Center circle
+
+        length = math.hypot(x2-x1, y2-y1)
+        #print(length)
+
+        if length <30:
+            cv2.circle(img, (cx,cy), 10, (0,255,0), cv2.FILLED) 
+
 
 
     cTime = time.time()
