f4465468d21f9ae1e0caea3e26dbd31c4709b477
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-06-29 22:31:27 +0530

    Files

--- Files/VolumeHandControl.py
+++ Files/VolumeHandControl.py
@@ -69,7 +69,7 @@ while True:
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f"FPS:{int(fps)}", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+    cv2.putText(img, f"FPS:{int(fps)}", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
